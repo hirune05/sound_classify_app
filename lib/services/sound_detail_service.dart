96f40ff3d1734f39a842733403a124718676747f
TajimaMiho <kapibaramihho@gmail.com>
2026-04-14 17:49:15 +0900

    ref: lat longをピントして表示

--- lib/services/sound_detail_service.dart
+++ lib/services/sound_detail_service.dart
@@ -3,13 +3,23 @@ import 'package:sound_classify_app/models/detail.dart';
 
 class SoundDetailService {
   final shopRef = FirebaseFirestore.instance.collection('shops');
+  final placeRef = FirebaseFirestore.instance.collection('places');
 
   Future<void> setSoundDetail(Detail soundDetail) async {
     final documentRef = shopRef.doc();
+    final lat = soundDetail.lat;
+    final long = soundDetail.long;
     await documentRef.set({
-      'lat': soundDetail.lat,
-      'long': soundDetail.long,
+      'lat': lat,
+      'long': long,
       'sounds': soundDetail.sounds,
     });
+
+    if (lat != null && long != null) {
+      await placeRef.add({
+        'lat': lat,
+        'long': long,
+      });
+    }
   }
 }
